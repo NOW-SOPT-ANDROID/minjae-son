7cc1f2cf7da9043eff32ba7b6519a26387e41a3c
SYAAINN <alswo08319@gmail.com>
2024-04-03 14:56:22 +0900

    [feat] LoginActivity 파일 Binding

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -1,5 +1,14 @@
 package com.sopt.now
 
 import android.os.Bundle
-class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.now.databinding.ActivityLoginBinding
+
+class LoginActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
