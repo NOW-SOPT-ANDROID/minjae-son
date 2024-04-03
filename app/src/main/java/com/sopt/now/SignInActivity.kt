883bbc3ae64c1c2382196746ebd8f9e4830a490d
SYAAINN <alswo08319@gmail.com>
2024-04-03 15:58:26 +0900

    [refactor] signin 파일 이름 변경

--- app/src/main/java/com/sopt/now/SignInActivity.kt
+++ app/src/main/java/com/sopt/now/SignInActivity.kt
@@ -2,14 +2,14 @@ package com.sopt.now
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sopt.now.databinding.ActivityJoinBinding
+import com.sopt.now.databinding.ActivitySigninBinding
 
-class JoinActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityJoinBinding
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
 
     fun OnCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityJoinBinding.inflate(layoutInflater)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
