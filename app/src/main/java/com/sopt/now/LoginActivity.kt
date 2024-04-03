139008cfb616d80f7d901755af30bf7ea185ea31
SYAAINN <alswo08319@gmail.com>
2024-04-03 15:15:16 +0900

    [feat] 로그인 성공여부 snackbar 구현

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.sopt.now
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
@@ -10,5 +12,15 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            if (binding.etSignInId.text.length >= 6) {
+                if (binding.etSignInPw.text.length in 6..9)
+                    Snackbar.make(it, "로그인 되었습니다!", Snackbar.LENGTH_SHORT).show()
+                else
+                    Snackbar.make(it,"비밀번호가 잘못되었습니다.",Snackbar.LENGTH_SHORT).show()
+            }
+            else Snackbar.make(it,"아이디가 잘못되었습니다.",Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
