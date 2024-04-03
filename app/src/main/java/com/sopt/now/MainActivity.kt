97d1ed6831fb0c7d2c925b51d979a6021e372119
SYAAINN <alswo08319@gmail.com>
2024-04-03 13:37:24 +0900

    [feat] 로그인 UI 회원가입 버튼 추가

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -7,9 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
