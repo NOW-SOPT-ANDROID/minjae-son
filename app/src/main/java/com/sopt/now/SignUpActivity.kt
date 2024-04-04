12f4f26daeb8e6d6085419f78d9c608c67a60915
SYAAINN <alswo08319@gmail.com>
2024-04-04 09:35:15 +0900

    [feat] 회원가입 성공 시 로그인 페이지로 이동 기능 추가

--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -1,34 +1,40 @@
 package com.sopt.now
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivitySignupBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
-
-    fun OnCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSignUp.setOnClickListener {
+
+            //    1. 모든 정보 입력되지 않았을 시, 회원 가입 불가 (snackbar , toast 등으로 메세지 표시)
+            //    2. 성공 시 LoginActivity로 화면 전환, 정보도 함께 전달, 회원가입 성공 메세지 표시
+            //    <회원가입 성공조건>
+            //    - ID : 6~10 글자
+            //    - Password : 8~12 글자
+            //    - 닉네임 : 한 글자 이상, 공백으로만 이루어진 닉네임은 불가
+            //    - 나머지 조건 : 자유
+
             if (binding.etSignUpId.text.length in 6..10 &&
                 binding.etSignUpPw.text.length in 8..12 &&
-                binding.etSignUpName.text.length >= 1) {
+                binding.etSignUpName.text.isNotEmpty()) {
                 Snackbar.make(it,"회원가입 완료!",Snackbar.LENGTH_SHORT).show()
+
+                val intent = Intent(this,LoginActivity::class.java) // 회원가입 성공 시, 로그인 화면으로 이동
+                startActivity(intent)
             }
             else
                 Snackbar.make(it,"회원가입 실패",Snackbar.LENGTH_SHORT).show()
         }
     }
 
-//    1. 모든 정보 입력되지 않았을 시, 회원 가입 불가 (snackbar , toast 등으로 메세지 표시)
-//    2. 성공 시 LoginActivity로 화면 전환, 정보도 함께 전달, 회원가입 성공 메세지 표시
-//    <회원가입 성공조건>
-//    - ID : 6~10 글자
-//    - Password : 8~12 글자
-//    - 닉네임 : 한 글자 이상, 공백으로만 이루어진 닉네임은 불가
-//    - 나머지 조건 : 자유
 }
