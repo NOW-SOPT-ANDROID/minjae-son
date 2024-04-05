e2c9a648f6a0d3a76edea7dcdf6895759848a876
SYAAINN <alswo08319@gmail.com>
2024-04-05 16:26:02 +0900

    [feat] 회원가입 정보와 비교 기능 구현

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -2,10 +2,9 @@ package com.sopt.now
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
@@ -15,18 +14,25 @@ class LoginActivity: AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val userId = intent.getStringExtra("ID")
+        val userPw = intent.getStringExtra("PW")
+        val userName = intent.getStringExtra("Name")
+        val userPlace = intent.getStringExtra("Place")
+
         binding.btnSignIn.setOnClickListener {
-            if (binding.etSignInId.text.length in 6..10) {
-                if (binding.etSignInPw.text.length in 8..12) {
+            if (binding.etSignInId.text.toString()== userId) {
+                if (binding.etSignInPw.text.toString() == userPw) {
                     Toast.makeText(this, "로그인 되었습니다!", Toast.LENGTH_SHORT).show()
 
-                    val intent = Intent(this,MainActivity::class.java) // 회원가입 페이지로 이동
+                    val intent = Intent(this,MainActivity::class.java) // 메인 페이지로 이동
                     startActivity(intent)
                 }
                 else
                     Toast.makeText(this, "비밀번호가 잘못되었습니다", Toast.LENGTH_SHORT).show()
             }
-            else Toast.makeText(this, "아이디가 잘못되었습니다!", Toast.LENGTH_SHORT).show()
+            else {
+                Toast.makeText(this, "아이디가 잘못되었습니다!", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.btnJoin.setOnClickListener {
--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivitySignupBinding
-
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +15,6 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSignUp.setOnClickListener {
-
             //    1. 모든 정보 입력되지 않았을 시, 회원 가입 불가 (snackbar , toast 등으로 메세지 표시)
             //    2. 성공 시 LoginActivity로 화면 전환, 정보도 함께 전달, 회원가입 성공 메세지 표시
             //    <회원가입 성공조건>
@@ -24,7 +22,6 @@ class SignUpActivity : AppCompatActivity() {
             //    - Password : 8~12 글자
             //    - 닉네임 : 한 글자 이상, 공백으로만 이루어진 닉네임은 불가
             //    - 나머지 조건 : 자유
-
             if (binding.etSignUpId.text.length in 6..10 &&
                 binding.etSignUpPw.text.length in 8..12 &&
                 binding.etSignUpName.text.isNotEmpty() &&
@@ -49,5 +46,4 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this,"회원가입 실패",Toast.LENGTH_SHORT).show()
         }
     }
-
 }
