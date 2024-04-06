92cf35f329eac05c38fb2bd61773db9c41591bc1
SYAAINN <alswo08319@gmail.com>
2024-04-06 16:33:48 +0900

    [feat] bottomNavigation 구현

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.sopt.now.databinding.ActivityLoginBinding
 import com.sopt.now.databinding.ActivityMainBinding
 
@@ -24,5 +25,42 @@ class MainActivity : AppCompatActivity() {
         binding.tvMainShowId.text = userId
         binding.tvMainShowPw.text = userPw
         binding.tvMainShowPlace.text = userPlace
+
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvHome.id)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fcvHome.id, HomeFragment())
+                .commit()
+        }
+    }
+    private fun clickBottomNavigation() {
+        binding.bnvHome.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.menu_home-> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_search-> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.menu_mypage-> {
+                    replaceFragment(MyPageFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    // Activity에서 Fragment를 다뤄야 하니 supportFragmentManager를 사용합니다.
+// 트렌젝션을 시작하고 replace 메서드를 통해 Fragment를 교체합니다.
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fcvHome.id, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/sopt/now/MyPageFragment.kt
@@ -0,0 +1,35 @@
+package com.sopt.now
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sopt.now.databinding.FragmentHomeBinding
+import com.sopt.now.databinding.FragmentMypageBinding
+
+class MyPageFragment : Fragment() {
+    private var _binding: FragmentMypageBinding? = null
+    private val binding get() = requireNotNull(_binding) {
+        "바인딩 객체 생성 해라"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View { // ? 를 지운이유 : binding.root 가 nullable 하지 않은걸 알아서
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 기존 액티비티의 onCreate에 작성했던 init로직을 이제는 여기에 작성합니다.
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
