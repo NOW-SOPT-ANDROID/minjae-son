eeaffd81ac8999eaf27d480aaf37debdb250e023
SYAAINN <alswo08319@gmail.com>
2024-05-25 00:10:32 +0900

    refactor: -

--- app/src/main/java/com/sopt/now/compose/main/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/main/home/HomeViewModel.kt
@@ -2,6 +2,8 @@ package com.sopt.now.compose.main.home
 
 import com.sopt.now.compose.R
 
+// 서버 통신 로직으로 대체
+
 val myProfile = Profiles.MyProfile(
     profileImage = R.drawable.sonminjae_profile,
     name = "Son Minjae",
